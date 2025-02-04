880110738121d7aa299ae461639f7e96cbc01e0b
nima-omidpour <nimaomidpour013@gmail.com>
2025-02-04 19:47:34 +0330

    Initial commit

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,14 +1,13 @@
-"use client"
+"use client";
 
-import { WalletDialog } from "@/components/wallet-dialog"
+import { WalletDialog } from "@/components/wallet-dialog";
 
 export function Header() {
   return (
     <header className="border-b border-zinc-800 bg-zinc-900/50 backdrop-blur-xl">
-      <div className="flex h-16 items-center justify-end px-4">
+      <div className=" flex h-16 items-center justify-end px-4">
         <WalletDialog />
       </div>
     </header>
-  )
+  );
 }
-
